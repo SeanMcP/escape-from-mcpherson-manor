6009ca0795a926343213448f877b8d4f2c3206cc
Sean McPherson <seanmcp25@gmail.com>
2019-01-09 13:25:02 -0500

    Add free-range navigation

---
+++ src/movement/position.js
@@ -0,0 +1,50 @@
+import { writeToScreen } from '../utils'
+
+export let currentPosition = [0, 0]
+export let positionHistory = [ [...currentPosition] ]
+
+function writePosition(direction) {
+    writeToScreen(`You travel ${direction}. Current location: ${currentPosition.join(', ')}`)
+}
+
+export function go(direction) {
+    let shouldPrintLocation = true
+    switch (direction) {
+        case 'north':
+        case 'n':
+            currentPosition[1]++
+            positionHistory.unshift([...currentPosition])
+            break
+        case 'east':
+        case 'e':
+            currentPosition[0]++
+            positionHistory.unshift([...currentPosition])
+            break
+        case 'south':
+        case 's':
+            currentPosition[1]--
+            positionHistory.unshift([...currentPosition])
+            break
+        case 'west':
+        case 'w':
+            currentPosition[0]--
+            positionHistory.unshift([...currentPosition])
+            break
+        case 'back':
+        case 'b':
+            if (positionHistory.length > 1) {
+                positionHistory = positionHistory.slice(1)
+                currentPosition = [...positionHistory[0]]
+            } else {
+                writeToScreen(`You cannot go back.`)
+                shouldPrintLocation = false
+            }
+            break
+        case undefined:
+            writeToScreen(`You must include a direction.`)
+            break
+        default:
+            return writeToScreen(`I don't recognize that direction.`)
+    }
+    if (direction && shouldPrintLocation) writePosition(direction)
+}
--- src/play.js
+++ src/play.js
@@ -1,15 +1,20 @@
 import { clearScreen, normalize, writeToScreen, writeUserInput } from './utils' 
+import { currentPosition, go } from './movement/position'
 
 function executeCommand(rawInput) {
     const input = normalize(rawInput)
+    const [command, ...rest] = input.split(' ')
 
     const commandRouter = {
         'clear': () => clearScreen(),
+        // This needs to be broken out into a separate function
+        'check': () => writeToScreen(`Current location: ${currentPosition}`),
+        'go': () => go(...rest),
         'sdg': () => writeToScreen('Soli Deo gloria!')
     }
 
-    if (commandRouter[input]) {
-        commandRouter[input]()
+    if (commandRouter[command]) {
+        commandRouter[command]()
     } else {
         writeToScreen('I don\'t understand.')
     }
